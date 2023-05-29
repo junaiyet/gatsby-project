007f84e76405ac0d0c3793c4d50e82667834ea7a
junaiyet <junaiyetmia.jm.bd@gmail.com>
2023-05-29 23:50:52 +0600

    code

--- src/components/RatedProductItem.js
+++ src/components/RatedProductItem.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import AiFillStar from 'react-icons/ai'
 export default function RatedProductItem({img}) {
   return (
 <div className='border overflow-hidden border-gray rounded-xl relative'>
@@ -11,7 +11,14 @@ export default function RatedProductItem({img}) {
         <div className=" text-center px-8">
             <p className='text-2xl pt-8 pb-2'>Wireless Headphone</p>
             <span className='inline-block text-2xl pb-4 text-primary font-medium'>$29.99</span>
-           
+           <div className="">
+            <span>5.00</span>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+           </div>
 
 
         </div>
